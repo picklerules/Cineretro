4d26dc01edbc5329375af7159381027b73f49813
picklerules <136508588+picklerules@users.noreply.github.com>
2024-02-19 06:40:04 -0500

    import accueil.json et css

--- src/components/Accueil/Accueil.js
+++ src/components/Accueil/Accueil.js
@@ -1,11 +1,14 @@
 import './Accueil.css';
+import accueilDonnees from './Accueil.json'
 
 function Accueil() {
   
   return (
     <main>
       {/*Le contenu du fichier accueil.json importé, placer accueil.json dans le composants accueil*/}
-        Accueil
+      {accueilDonnees.map((p, index) => (
+        <p key={index}>{p}</p>
+      ))}
     </main>
 )
 
