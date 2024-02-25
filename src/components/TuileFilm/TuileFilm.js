b7a449cf26c4cd3700ee11fe1ab5eed27cd73501
picklerules <136508588+picklerules@users.noreply.github.com>
2024-02-25 11:48:47 -0500

    ajuster la taille des tuiles

--- src/components/TuileFilm/TuileFilm.js
+++ src/components/TuileFilm/TuileFilm.js
@@ -5,9 +5,11 @@ function TuileFilm({ data, filtreActif }) {
   return (
     <article className="grid__item">
       <img src={`/img/${data.titreVignette}`} alt={data.titre} />
+
       <h3>{data.titre}</h3>
       {filtreActif.champ === 'realisation' && <p>Réalisateur: {data.realisation}</p>}
       {filtreActif.champ === 'annee' && <p>Année: {data.annee}</p>}
+
     </article>
   );
 }
